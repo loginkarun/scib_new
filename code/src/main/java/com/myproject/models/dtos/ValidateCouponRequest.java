f70b84f1c6b1c6e669f591a16a7d9f56fc2a8856
loginkarun <loginkarun@gmail.com>
2026-03-05 16:31:33 +0530

    Auto commit: Scripts committed on 2026-03-05-11-01-32

--- code/src/main/java/com/myproject/models/dtos/ValidateCouponRequest.java
+++ code/src/main/java/com/myproject/models/dtos/ValidateCouponRequest.java
@@ -2,15 +2,13 @@ package com.myproject.models.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * DTO for validating coupon code
+ * DTO for validating coupon request.
  */
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ValidateCouponRequest {
