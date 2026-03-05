21563005b3664b3ac758e52735162052b753326c
loginkarun <loginkarun@gmail.com>
2026-03-05 16:31:46 +0530

    Auto commit: Scripts committed on 2026-03-05-11-01-46

--- code/src/main/java/com/myproject/models/dtos/CartResponse.java
+++ code/src/main/java/com/myproject/models/dtos/CartResponse.java
@@ -1,17 +1,15 @@
 package com.myproject.models.dtos;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 /**
- * DTO for cart response
+ * DTO for cart response.
  */
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartResponse {
